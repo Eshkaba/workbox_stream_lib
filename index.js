dfdc0792d3088651fb5b4c7fbe0fc2488da73fde
Front-end Developer <118537409+Eshkaba@users.noreply.github.com>
2023-12-27 13:22:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 /*
-  Copyright 2018 Google LLC
-
-  Use of this source code is governed by an MIT-style
-  license that can be found in the LICENSE file or at
-  https://opensource.org/licenses/MIT.
+  
 */
 import { concatenate } from './concatenate.js';
 import { concatenateToResponse } from './concatenateToResponse.js';
